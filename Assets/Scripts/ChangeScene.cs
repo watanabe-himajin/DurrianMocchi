0792133c01530dea76236f0140ec65712e13b6a2
watanabe-himajin <c612201036@tokushima-u.ac.jp>
2024-02-22 15:56:10 +0800

    Carp

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public static int Coin = 0;
     // Start is called before the first frame update
     public void ChangeSelectStage()
     {
