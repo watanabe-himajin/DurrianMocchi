2e671fe765c3c4e6cbf666500b79b57ad6612a4c
watanabe-himajin <c612201036@tokushima-u.ac.jp>
2024-02-22 13:47:24 +0800

    We can change stage!

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeSelectStage()
+    {
+        SceneManager.LoadScene("SelectStage");
+    }
+
+    public void ChangeTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void ChangeRiver()
+    {
+        SceneManager.LoadScene("River");
+    }
+
+    public void ChangeSwamp()
+    {
+        SceneManager.LoadScene("Swamp");
+    }
+
+    public void ChangeSea()
+    {
+        SceneManager.LoadScene("Sea");
+    }
+
+    public void ChangeTestStage()
+    {
+        SceneManager.LoadScene("TestStage");
+    }
+}
